70aff31aa6c7f46f083c32c4c896f892961df8c1
minki3 <86992006+minki3@users.noreply.github.com>
2023-04-27 20:04:21 +0900

    ADD: postpage markdown 적용

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,8 @@
 import { getCardData, getPost } from "@/app/service/products";
 import { notFound, redirect } from "next/navigation";
+import MarkdownViewer from "./markdownViewer";
+import Image from "next/image";
+import { AiTwotoneCalendar } from "react-icons/ai";
 
 interface Props {
   params: {
@@ -15,15 +18,31 @@ export function generateMetadata({ params }: Props) {
 
 export default async function PostsPage({ params: { slug } }: Props) {
   const post = await getPost(slug);
+  const { title, description, date, category, path, featured, content } = post;
   console.log(post);
   if (!post) {
     return notFound();
   }
   return (
-    <section>
-      <h1>{post.title}</h1>
-      <pre>{post.content}</pre>
-    </section>
+    <article className=" rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+      <Image
+        className="w-full h-1/5 max-h-[500px]"
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className="flex flex-col p-4">
+        <div className="flex items-center self-end text-sky-600">
+          <AiTwotoneCalendar />
+          <p className="font-semibold ml-2">{date.toString()}</p>
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-bold">{description}</p>
+        <div className="w-44 border-2 border-sky-600 mt-4 mb-8" />
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
 
